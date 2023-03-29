6c2f56690b4b3638f2ab289f308d9793e318affd
Gergo Szalai <szerg97@gmail.com>
2023-03-29 17:33:38 +0200

    configured exchange service for distributed tracing, zipkin

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -3,6 +3,8 @@ package com.microservices.currencyexchangeservice.controller;
 import com.microservices.currencyexchangeservice.model.CurrencyExchange;
 import com.microservices.currencyexchangeservice.service.CurrencyExchangeService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CurrencyExchangeController {
 
+    private final Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
     private final CurrencyExchangeService service;
 
     @GetMapping("/from/{from}/to/{to}")
@@ -20,6 +23,7 @@ public class CurrencyExchangeController {
             @PathVariable String from,
             @PathVariable String to
     ){
+        logger.info("Get exchange was called: from {} to {}", from, to);
         return service.getOne(from, to);
     }
 }
